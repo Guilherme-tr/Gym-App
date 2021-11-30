19358a649aea10e9122a89ee87d4fcad8fe18092
Guilherme-tr <gui.trujilo.wecomp@gmail.com>
2021-11-29 23:28:01 -0300

    integrando o campo senha e esqueceu a senha

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,49 +14,91 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 50),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [topColor, bottomColor])),
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/fitness.png",
-              height: 125,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [topColor, bottomColor])),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  "assets/fitness.png",
+                  height: 125,
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                  bottom: 15,
+                )),
+                Text(
+                  "Entrar",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          labelText: "E-mail",
+                          labelStyle: TextStyle(color: Colors.white),
+                          prefixIcon: Icon(
+                            Icons.mail_outline,
+                            color: Colors.white,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Senha",
+                          labelStyle: TextStyle(color: Colors.white),
+                          prefixIcon: Icon(
+                            Icons.vpn_key_outlined,
+                            color: Colors.white,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Esqueceu a senha?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                    textAlign: TextAlign.right,
+                  ),
+                )
+              ],
             ),
-            Padding(
-                padding: EdgeInsets.only(
-              bottom: 15,
-            )),
-            Text(
-              "Entrar",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(
-                      Icons.mail_outline,
-                    )),
-                  )
-                ],
-              ),
-            )
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-        ),
-      ),
+          )),
     );
   }
 }
